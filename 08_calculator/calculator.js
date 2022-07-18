9df9377693771aec6b11d065324cfcd03c647963
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 18:13:29 -0400

    Finish exercise 08 calculator
    used loops

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  let final = 0;
+	for (let i = 0; i<array1.length; i++){
+    final += array1[i];
+  };
+  return final;
 };
 
-const multiply = function() {
-
+const multiply = function(listNums) {
+  let result = 1;
+  for (let i = 0; i< listNums.length; i++){
+    result *= listNums[i];
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	let result = 1;
+  for (let i = 0; i<pow; i++){
+    result *= base
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0){
+    return 1
+  }
+  let result = 1
+  for ( i = num; i>0; i--){
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
