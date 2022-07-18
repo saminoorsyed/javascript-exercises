113e1f6b4d0f56707af5a303434d890eea02e27d
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 17:11:37 -0400

    Finish exercise 07
    Used a helper function and Math.round to get the nearest 10th

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return roundTo10th((temp-32)*(5/9))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return roundTo10th((temp*(9/5))+32)
 };
+
+const roundTo10th = function(num) {
+  return Math.round(num *10)/10;
+}
 
 // Do not edit below this line
 module.exports = {
