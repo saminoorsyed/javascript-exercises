9e0c319b99cf505f0256f5d0ff7629e71a968590
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 14:06:45 -0400

    finish exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let str = string;
+    for (let i = 0; i< number; i++){
+        str += str;
+    };
+    return str;
 };
 
 // Do not edit below this line
