c8f1e0b1fe2c419353fb02069bbcff9e8423e915
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 16:49:22 -0400

    Finish exercise 5
    used && as well as || conditionals along with typeof operator

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (typeof number1 != typeof number2 || typeof number1 != 'number'){
+        return 'ERROR'
+    } 
 
+    if (number1 < 0 || number2 <0) {
+        return 'ERROR'
+    } 
+    return ((number1+number2)/2)*(Math.abs((number1-number2))+1)
 };
 
 // Do not edit below this line
