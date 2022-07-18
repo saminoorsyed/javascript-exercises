f370039fdfba2b41c9a47b36303c97282c0ef3e7
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 19:06:11 -0400

    Finished exercise 10
    not hard, will update to include dynamic programming

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let nums = parseInt(num)
+    if (nums === NaN || nums < 0){
+        return "OOPS"
+    }else if (nums == 1){
+        return 1
+    }else if (nums == 2){
+        return 1
+    }else {
+        return fibonacci(nums-1)+fibonacci(nums-2)
+    }
 };
 
 // Do not edit below this line
