116d1c1704fd00fe51fdf7d3d3879fda6bb3c651
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 19:16:34 -0400

    Adjust fibonacci algo to be dynamic O(n)time&space
    Use bottom up approach

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,14 +1,15 @@
 const fibonacci = function(num) {
-    let nums = parseInt(num)
+    let nums = parseInt(num);
     if (nums === NaN || nums < 0){
-        return "OOPS"
-    }else if (nums == 1){
-        return 1
-    }else if (nums == 2){
-        return 1
-    }else {
-        return fibonacci(nums-1)+fibonacci(nums-2)
+        return "OOPS";
+    };
+    let cache = Array(nums+2);
+    cache[0] = 0;
+    cache[1] = 1;
+    for (let i = 2; i <= nums; i++){
+        cache[i] = cache[i-1]+cache[i-2];
     }
+    return cache[nums]
 };
 
 // Do not edit below this line
