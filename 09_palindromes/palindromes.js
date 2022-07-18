1484da72ba248e954e552ed46aa031299ff4c369
saminoorsyed <saminoorsyed@gmail.com>
2022-07-18 18:52:29 -0400

    Finish exercise 09
    Had a little trouble with the palindrome and removing nonalphanumeric
    characters.
    Also having trouble with what order to put methods in and what their
    outcomes might be. (ie. reverse())

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let first = input.replace(/[^a-z0-9]/gi,'').toLowerCase();
+    let second = first.split('').reverse().join('');
+    return (first === second)
 };
 
 // Do not edit below this line
