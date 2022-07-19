1ca38940eebaa01ef1cd009920ef4a247f1d03da
saminoorsyed <saminoorsyed@gmail.com>
2022-07-19 14:11:22 -0400

    Finish exercise 12
    iterate through a list and find find a specific value stored, return
    the largest. used javascript date function slice and splice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function(people) {
-    people.reduce(function(previousValue, currentValue))
+const getAge = function(person) {
+    const currentYear = new Date().getFullYear();
+    return (person.yearOfDeath ? person.yearOfDeath : currentYear) - person.yearOfBirth
 };
+
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    people = (people.slice()).splice(1);
+
+    for (let person of people) {
+        if (getAge(person)> getAge(oldestPerson)){
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
