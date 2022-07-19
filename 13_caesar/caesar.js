1f043186142c41ced070407bdac5c5df99ab2e2d
saminoorsyed <saminoorsyed@gmail.com>
2022-07-19 15:40:59 -0400

    Finish exercise 13
    clever solution, fun to see how others handled the problem

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
-
+const caesar = function(readable,cipher) {
+    const confidential = readable.split("");
+    
+    for (let i = 0; i< confidential.length; i++) {
+        if ((/[a-zA-Z]/).test(confidential[i])){
+            const start = confidential[i] === confidential[i].toLowerCase() ? "a".charCodeAt(0) : "A".charCodeAt(0);
+            const diff = confidential[i].charCodeAt(0) - start;
+            const shift = cipher >=0 ? diff + cipher : diff + Math.abs(26 - Math.abs(cipher%26));
+            confidential[i] = String.fromCharCode(shift % 26 + start);
+        }
+    }
+    return confidential.join("")
 };
 
 // Do not edit below this line
